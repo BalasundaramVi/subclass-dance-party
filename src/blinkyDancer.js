b7f0a8961333d3942223ec439ce416ef0e8362dc
BalasundaramVi <vigneshbalasundaram@gmail.com>
2018-09-24 15:02:59 -0700

    Add dancer and blinky dancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -17,3 +17,19 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
   return blinkyDancer;
 };
+
+var makeBlinkyDancer = function (top, left, timeBetweenSteps) {
+  this.timeBetweenSteps = timeBetweenSteps;
+  makeDancer.call(this, top, left, this.timeBetweenSteps);
+}
+
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+
+makeBlinkyDancer.prototype.oldStep = makeDancer.prototype.step;
+
+makeBlinkyDancer.prototype.step = function() {
+  this.oldStep();
+  this.$node.toggle();
+}
